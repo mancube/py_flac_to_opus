db37108c0e4376629016c2c6820199a789c27065
Ilija <ilijasrb@gmail.com>
2023-11-10 11:44:04 +0100

    novija radna verzija

--- backend.py
+++ backend.py
@@ -1,9 +1,7 @@
 from pydub import AudioSegment
-from pydub.utils import mediainfo
-from mutagen import File
 import os
-import subprocess
 import shutil
+import mutagen
 import tempfile
 
 def flac_to_opus(input_file, output_file):
@@ -25,15 +23,28 @@ def flac_to_opus(input_file, output_file):
 
 def copy_tags(input_file, output_file):
     # Load FLAC tags
-    flac_tags = File(input_file)
-
+    flac_tags = mutagen.File(input_file)
+    if flac_tags is None:
+        return
+    
     # Copy FLAC tags to OPUS file
-    opus_tags = File(output_file, easy=True)
+    opus_tags = mutagen.File(output_file, easy=True)
+    if opus_tags is None:
+        return
+    
+    # Exclude the "LYRICS" tag
+    if "LYRICS" in flac_tags:
+        del flac_tags["LYRICS"]
+    elif "lyrics" in flac_tags:
+        del flac_tags["lyrics"]
+    
+    opus_tags.clear()
     opus_tags.update(flac_tags)
+    opus_tags.save()
 
 def copy_cover(input_file, output_file):
     # Load FLAC cover
-    flac_tags = File(input_file)
+    flac_tags = mutagen.File(input_file)
     flac_cover = flac_tags.tags.get("APIC:")
     
     if flac_cover:
@@ -47,28 +58,34 @@ def copy_cover(input_file, output_file):
 
         # Remove temporary cover file
         os.remove(temp_cover_file)
+    
+    # Check if there is a cover file with any case variation in the input directory
+    input_dir = os.path.dirname(input_file)
+    for file in os.listdir(input_dir):
+        if file.lower() == "cover.jpg":
+            cover_file = os.path.join(input_dir, file)
+            # Copy the cover file to the output directory
+            shutil.copy(cover_file, os.path.dirname(output_file))
+            return
+        
+def convert_file(input_file, output_folder):
+    output_file = os.path.join(output_folder, os.path.basename(input_file).replace(".flac", ".opus"))
+    flac_to_opus(input_file, output_file)
+    copy_tags(input_file, output_file)
+    copy_cover(input_file, output_file)
 
 def batch_convert_folder(input_folder, output_folder):
     # Create output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
 
     # Iterate through all FLAC files in the input folder
+    flac_files = []
     for root, dirs, files in os.walk(input_folder):
         for file in files:
             if file.lower().endswith(".flac"):
-                input_file = os.path.join(root, file)
-                output_file = os.path.join(output_folder, file.replace(".flac", ".opus"))
+                flac_files.append(os.path.join(root, file))
 
-                # Convert and copy tags and cover
-                flac_to_opus(input_file, output_file)
-                copy_tags(input_file, output_file)
-                copy_cover(input_file, output_file)
-
-if __name__ == "__main__":
-    # Replace 'input_folder' with the path to your folder containing FLAC files
-    input_folder = "path/to/flac/folder"
-
-    # Replace 'output_folder' with the desired path for the output OPUS files
-    output_folder = "path/to/output/folder"
-
-    batch_convert_folder(input_folder, output_folder)
+    # Convert each FLAC file to OPUS
+    for file in flac_files:
+        convert_file(file, output_folder)
+    print("Batch conversion completed.")
--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
+import json
 from backend import batch_convert_folder
+
+CONFIG_FILE = "config.json"
 
 def choose_input_folder(entry):
     folder = filedialog.askdirectory()
@@ -15,8 +18,22 @@ def choose_output_folder(entry):
 def convert_button_click(input_folder, output_folder):
     batch_convert_folder(input_folder, output_folder)
     tk.messagebox.showinfo("Conversion Complete", "FLAC to OPUS conversion is complete.")
+    
+def load_config():
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            config = json.load(config_file)
+            return config
+    except FileNotFoundError:
+        return {}
+
+def save_config(config):
+    with open(CONFIG_FILE, "w") as config_file:
+        json.dump(config, config_file)
 
 def create_gui():
+    config = load_config()
+    
     root = tk.Tk()
     root.title("FLAC to OPUS Converter")
 
@@ -37,10 +54,20 @@ def create_gui():
 
     output_folder_button = tk.Button(root, text="Choose Folder", command=lambda: choose_output_folder(output_folder_entry))
     output_folder_button.grid(row=1, column=2, padx=5, pady=5)
+    
+    if "input_folder" in config:
+        input_folder_entry.insert(0, config["input_folder"])
+    if "output_folder" in config:
+        output_folder_entry.insert(0, config["output_folder"])
+        
+    def save_folders():
+        config["input_folder"] = input_folder_entry.get()
+        config["output_folder"] = output_folder_entry.get()
+        save_config(config)
 
-    convert_button = tk.Button(root, text="Convert", command=lambda: convert_button_click(
+    convert_button = tk.Button(root, text="Convert", command=lambda: [convert_button_click(
         input_folder_entry.get(), output_folder_entry.get()
-    ))
+    ), save_folders()])
     convert_button.grid(row=3, column=0, columnspan=3, pady=10)
 
     root.mainloop()
